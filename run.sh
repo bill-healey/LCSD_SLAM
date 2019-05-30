cc4c0bd8422ccb5aa8cf186b169f8d0cef152f0f
Bill Healey <bill@elacarte.com>
2019-05-30 13:53:15 -0700

    Updating to OpenCV3

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+killall roscore
+killall Mono
+sleep 4
+screen -dmS core bash -c 'roscore'
+sleep 4
+screen -dmS orb bash -c 'cd ORB_SLAM2; rosrun ORB_SLAM2 Mono Vocabulary/ORBvoc.txt Examples/Monocular/TUMmonoVO_yaml/monoVO_ORB_SLAM_full_1.yaml'
+sleep 4
+#screen -dmS dso bash -c 'cd DSO_ROS/catkin_ws; . devel/setup.bash; roslaunch dso_ros monoVO_seong_01.launch'
+cd DSO_ROS/catkin_ws;
+. devel/setup.bash;
+roslaunch dso_ros monoVO_seong_01.launch
